40fd0c80dd545b5e68d23b5cf177fc1aec93d4ce
Jabulani-N <jabu.94@gmail.com>
2024-01-23 14:18:20 -0600

    task0: added refresher notes about basic python structure

--- supervised_learning/classification/0-neuron.py
+++ supervised_learning/classification/0-neuron.py
@@ -1,1 +1,31 @@
 #!/usr/bin/env python3
+
+"""module documentation
+
+this module, 0-neuron.py, creates a class: Neuron
+
+"""
+
+
+class Neuron:
+    """Object-class summary documentation.
+
+
+    attributes will be documented in the below section
+    indent them like anything else protected by a colon.
+
+    Attributes:
+
+    """
+
+    pass
+
+    def __init__(self):
+        """__init__ method documentation
+
+        __init__ can optionally instead be documented in the class section
+        do not include 'self' as an arg
+
+        'Args:' would go here
+        """
+        pass
