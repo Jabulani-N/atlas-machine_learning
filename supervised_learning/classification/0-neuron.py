32d3a94e22cb1d63ca62e7746207314b0e6f7ccc
Jabulani-N <jabu.94@gmail.com>
2024-01-24 15:37:23 -0600

    task0: style improvements

--- supervised_learning/classification/0-neuron.py
+++ supervised_learning/classification/0-neuron.py
@@ -7,14 +7,16 @@ import numpy as np
 this module, 0-neuron.py, creates a class: Neuron
 """
 
+
 class Neuron:
     """Object-class summary documentation.
 
     Attributes:
         Public instance attributes:
-        	W: weights. Upon instantiation, initialized using random normal distribution.
-        	b: bias of the neuron. Upon instantiation,initialized to 0.
-        	A: output. Upon instantiation,initialized to 0.
+            W: weights. Upon instantiation, initialized to
+                random normal distribution.
+            b: bias of the neuron. Upon instantiation,initialized to 0.
+            A: output. Upon instantiation,initialized to 0.
     """
 
     pass
@@ -26,7 +28,7 @@ class Neuron:
         do not include 'self' as an arg
 
         nx is the number of input features to the neuron
-			If nx is not  integer, raise TypeError: "nx must be an integer"
+            If nx is not  integer, raise TypeError: "nx must be an integer"
             If nx < 1, raise ValueError: "nx must be a positive integer"
             IN THAT ORDER
 
@@ -43,6 +45,6 @@ class Neuron:
         self.W = np.random.normal(size=(1, nx))
         # had to reference myself to figure out this line.
         # don't remember original source
-        #	treat as source model in future references
+        #   treat as source model in future references
         self.b = 0
         self.A = 0
