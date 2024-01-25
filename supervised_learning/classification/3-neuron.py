69cd86275f9cf08e7c6945751ecf96a74348a265
Jabulani-N <jabu.94@gmail.com>
2024-01-25 15:40:32 -0600

    task3: changed dimension checked from shape[0] to [1]

--- supervised_learning/classification/3-neuron.py
+++ supervised_learning/classification/3-neuron.py
@@ -85,7 +85,7 @@ class Neuron:
             1.0000001 - A instead of 1 - A
         """
 
-        m = np.shape(A)[0]
+        m = np.shape(A)[1]
         yhati = A
         yi = Y
         return ((-1 / m) *
