a27b1c30ba935be7b1820089fcbf915ce88b7743
Jabulani-N <jabu.94@gmail.com>
2024-01-31 17:14:03 -0600

    task 25 created

---
+++ supervised_learning/classification/25-one_hot_decode.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""this module contains a function
+one hot decode that converts from an aray of categories to
+an array of 'is it category 1? is it category 2?...'
+"""
+
+import numpy as np
+
+
+def one_hot_decode(one_hot):
+    """recieves an encoded array; turns it into a vector of labels"""
+    pass
