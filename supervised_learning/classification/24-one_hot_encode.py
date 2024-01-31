29604eb3105682231e33fe679e43b8b227924ac2
Jabulani-N <jabu.94@gmail.com>
2024-01-31 17:05:55 -0600

    task 24 relocated import to avoid redundant imports

--- supervised_learning/classification/24-one_hot_encode.py
+++ supervised_learning/classification/24-one_hot_encode.py
@@ -4,15 +4,15 @@ one hot encode that converts from an aray of categories to
 an array of 'is it category 1? is it category 2?...'
 """
 
+import numpy as np
+
 
 def one_hot_encode(Y, classes):
     """binarizes an array Y
     Y = array of class numbers (int)
     classes = number of classes within Y
-        seems to be the length of Y.
+        seems to be the length of Y
     """
-
-    import numpy as np
 
     biggestPossibleClassNumber = max(Y)
     parent = np.zeros((classes, len(Y)))
