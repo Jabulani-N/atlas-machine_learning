6c3512253d16c75372a6eeaf2c56cff66dff5bb1
Jabulani-N <jabu.94@gmail.com>
2024-02-02 13:08:09 -0600

    task 0 populated

--- supervised_learning/tensorflow/0-create_placeholders.py
+++ supervised_learning/tensorflow/0-create_placeholders.py
@@ -1,10 +1,23 @@
 #!/usr/bin/env python3
 """module documentation
-this module,0-create_placeholders, creates a class: create_placeholders
+this module,0-create_placeholders, creates a function:
+    create_placeholders
+Though classy, it has no class.
 
 """
 
 
+import tensorflow.compat.v1 as tf
+
+
 def create_placeholders(nx, classes):
-    """returns two placeholders for the neural network"""
-    pass
+    """returns two placeholders for the neural network
+    nx: the number of feature columns in our data
+    classes: the number of classes in our classifier
+
+    x = placeholder for input data
+    y = placeholderfor one-hot labels
+    """
+    x = nx
+    y = classes
+    return x, y
