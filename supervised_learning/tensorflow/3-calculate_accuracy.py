task3: calculation syntax errors fixed

supervised_learning/tensorflow/3-calculate_accuracy.py
@@ -20,5 +20,5 @@ def calculate_accuracy(y, y_pred):
 
     return tf.divide(tf.math.reduce_sum(
             tf.cast(tf.math.equal(y, y_pred),
-                    tf.float32),
-            len(y)))
+                    tf.float32)),
+                     tf.cast(tf.size(y), tf.float32))