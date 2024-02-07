4de86b6e2b77ea11f2da3e73c734f3b8d213d30a
Jabulani-N <jabu.94@gmail.com>
2024-02-07 15:22:06 -0600

    task 6 prepared imports, created documentation

--- supervised_learning/tensorflow/6-train.py
+++ supervised_learning/tensorflow/6-train.py
@@ -6,10 +6,25 @@ this module, creates a function:
         operation that minimizes loss of a tensor set
 Though classy, it has no class.
 
+X_train is a numpy.ndarray containing the training input data
+Y_train is a numpy.ndarray containing the training labels
+X_valid is a numpy.ndarray containing the validation input data
+Y_valid is a numpy.ndarray containing the validation labels
+
+layer_sizes is a list containing the number of nodes in each layer of the network
+activations is a list containing the activation functions for each layer of the network
+alpha is the learning rate
+iterations is the number of iterations to train over
+save_path designates where to save the model
 """
 
 
 import tensorflow.compat.v1 as tf
+calculate_accuracy = __import__('3-calculate_accuracy').calculate_accuracy
+calculate_loss = __import__('4-calculate_loss').calculate_loss
+create_placeholders = __import__('0-create_placeholders').create_placeholders
+create_train_op = __import__('5-create_train_op').create_train_op
+forward_prop = __import__('2-forward_prop').forward_prop
 
 
 def train(X_train, Y_train, X_valid, Y_valid,
