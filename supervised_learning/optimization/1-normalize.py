979ac78ac70a9417af6972c86f10c14b07f99d6e
Jabulani-N <jabu.94@gmail.com>
2024-02-13 16:36:58 -0600

    created task 1

---
+++ supervised_learning/optimization/1-normalize.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""module documentation
+this module,1-normalize, creates a function:
+    normlaize
+Though classy, it has no class.
+
+"""
+
+
+import numpy as np
+
+
+def normalize(X, m, s):
+    """returns the normalized array of X
+    X = input numpy.ndarray shape (d, nx)
+        d = number of data points
+        nx = number of features
+    m = numpy.ndarray shape (nx,)
+        contains means of each X feature
+    s = numpy.ndarray shape (nx,)
+        contains standard devs of each X feature
+    """
