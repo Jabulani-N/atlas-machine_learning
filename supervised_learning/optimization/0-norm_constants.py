e0aaf272e3be95be8671982add3b968f68c4e735
Jabulani-N <jabu.94@gmail.com>
2024-02-12 21:12:49 -0600

    task 0 documentation improvements

--- supervised_learning/optimization/0-norm_constants.py
+++ supervised_learning/optimization/0-norm_constants.py
@@ -13,5 +13,8 @@ import numpy as np
 def normalization_constants(X):
     """This function will calculate and return
     mean, standard deviation
+    X is numpy aray with shape (m, nx)
+        m = number of data points
+        nx = number of features  (in each data point?)
     """
     return np.mean(X, axis=0), np.std(X, axis=0)
