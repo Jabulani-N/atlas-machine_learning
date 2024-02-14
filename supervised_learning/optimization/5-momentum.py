4668341bef0a8c3cfd6592ab69207ca19271fe0e
Jabulani-N <jabu.94@gmail.com>
2024-02-14 00:46:44 -0600

    task 5 updated variable is now calculated using the newly calculated  moment instead of the old input moment

--- supervised_learning/optimization/5-momentum.py
+++ supervised_learning/optimization/5-momentum.py
@@ -14,8 +14,10 @@ def update_variables_momentum(alpha, beta1, var, grad, v):
 `var` = numpy.ndarray containing the variable to be updated
 `grad` = numpy.ndarray containing the gradient of var
 `v` = previous first moment of var
+moment = the new moment for for the updated_var
+updated_var: variable updated via gradient descent
 """
     moment = np.add(np.multiply(beta1, v),
                     np.multiply((1 - beta1), grad))
-    updated_var = np.subtract(var, np.multiply(alpha, v))
+    updated_var = np.subtract(var, np.multiply(alpha, moment))
     return updated_var, moment
