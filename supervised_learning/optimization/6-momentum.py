500278e14d64a76d58a7985d6d7dd7f262cf0a1c
Jabulani-N <jabu.94@gmail.com>
2024-02-14 02:09:14 -0600

    task 6 indented documentatoin

--- supervised_learning/optimization/6-momentum.py
+++ supervised_learning/optimization/6-momentum.py
@@ -10,8 +10,8 @@ import tensorflow.compat.v1 as tf
 
 def create_momentum_op(loss, alpha, beta1):
     """loss = loss of the network
-alpha = learning rate
-beta1 = momentum weight
-"""
+    alpha = learning rate
+    beta1 = momentum weight
+    """
     optimizer = tf.train.MomentumOptimizer(alpha, beta1).minimize(loss)
     return optimizer
