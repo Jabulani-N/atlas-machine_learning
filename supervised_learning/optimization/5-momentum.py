277128b76154d05805c750fd6a4098f91b57a890
Jabulani-N <jabu.94@gmail.com>
2024-02-14 00:17:07 -0600

    task 5 created

---
+++ supervised_learning/optimization/5-momentum.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""module documentation
+this module creates a function:
+    moving_average
+
+"""
+
+
+import numpy as np
+
+
+def update_variables_momentum(alpha, beta1, var, grad, v):
+    """placeholder documentation"""
+    pass
