fdd8b7a3fd31d41552b556c0a58e25adb9f1c61b
Jabulani-N <jabu.94@gmail.com>
2024-02-14 00:37:22 -0600

    task 5 style improvement

--- supervised_learning/optimization/5-momentum.py
+++ supervised_learning/optimization/5-momentum.py
@@ -15,7 +15,7 @@ def update_variables_momentum(alpha, beta1, var, grad, v):
 `grad` = numpy.ndarray containing the gradient of var
 `v` = previous first moment of var
 """
-    moment = np.add(np.multiply(beta1, v ),
+    moment = np.add(np.multiply(beta1, v),
                     np.multiply((1 - beta1), grad))
     updated_var = np.subtract(var, np.multiply(alpha, v))
     return moment, updated_var
