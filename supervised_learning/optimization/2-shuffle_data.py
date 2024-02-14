f0e2c9c6f646ac54503b2ca7e3afc0f7a849d807
Jabulani-N <jabu.94@gmail.com>
2024-02-13 19:21:56 -0600

    created task 2

---
+++ supervised_learning/optimization/2-shuffle_data.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""module documentation
+this module creates a function:
+    shuffle_data
+Though classy, it has no class.
+
+"""
+
+
+import numpy as np
+
+def shuffle_data(X, Y):
+    """placeholder documentation"""
+    pass
