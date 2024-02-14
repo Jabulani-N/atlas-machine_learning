9855faa02161f292557003a265608805bcb43587
Jabulani-N <jabu.94@gmail.com>
2024-02-14 02:05:43 -0600

    task 6 minor documentation change

--- supervised_learning/optimization/6-momentum.py
+++ supervised_learning/optimization/6-momentum.py
@@ -12,6 +12,7 @@ def create_momentum_op(loss, alpha, beta1):
     """loss = loss of the network
 alpha = learning rate
 beta1 = momentum weight
+
 """
     optimizer = tf.train.MomentumOptimizer(alpha, beta1).minimize(loss)
     return optimizer
