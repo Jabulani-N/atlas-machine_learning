fb08d8b64d962be5744e5519043b1b438ff85990
Jabulani-N <jabu.94@gmail.com>
2024-02-14 03:09:39 -0600

    task 7 filled with the forumua

--- supervised_learning/optimization/7-RMSProp.py
+++ supervised_learning/optimization/7-RMSProp.py
@@ -9,5 +9,18 @@ import numpy as np
 
 
 def update_variables_RMSProp(alpha, beta2, epsilon, var, grad, s):
-    """placeholder documentation"""
-    pass
+    """`alpha` = learning rate
+    `beta2` = RMSProp weight
+    `epsilon` = small number to avoid division by zero
+    `var` = numpy.ndarray containing the variable to be updated
+    `grad` = numpy.ndarray containing the gradient of var
+    `s` = previous second moment of var
+    """
+    moment2 = np.multiply(beta2, np.add(s, (1 - beta2) * np.square(grad)))
+    # Update var using the NEW second moment
+    updated_var = np.subtract(var,
+                              np.multiply(alpha,
+                                          (np.divide(grad,
+                                                     (np.add(np.sqrt(moment2),
+                                                             epsilon))))))
+    return updated_var, moment2
