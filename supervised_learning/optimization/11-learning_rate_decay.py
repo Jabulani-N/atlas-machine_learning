2094bf7b973c09135b626f52fc7665fff35dc233
Jabulani-N <jabu.94@gmail.com>
2024-02-14 18:32:13 -0600

    task 11 tested successfully

--- supervised_learning/optimization/11-learning_rate_decay.py
+++ supervised_learning/optimization/11-learning_rate_decay.py
@@ -18,4 +18,6 @@ def learning_rate_decay(alpha, decay_rate, global_step, decay_step):
     the learning rate decay should occur in a stepwise fashion
     Returns: the updated value for alpha
     """
-    pass
+    alpha_new = alpha / (1 + decay_rate *
+                         (global_step // decay_step))
+    return alpha_new
