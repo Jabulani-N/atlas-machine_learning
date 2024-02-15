ecb7d145de98dbae08892c8d4caa001f2405a271
Jabulani-N <jabu.94@gmail.com>
2024-02-14 18:51:49 -0600

    task 12 style improvement

--- supervised_learning/optimization/12-learning_rate_decay.py
+++ supervised_learning/optimization/12-learning_rate_decay.py
@@ -5,7 +5,7 @@ this module creates a function.
 """
 
 
-import  tensorflow.compat.v1 as tf
+import tensorflow.compat.v1 as tf
 
 
 def learning_rate_decay(alpha, decay_rate, global_step, decay_step):
