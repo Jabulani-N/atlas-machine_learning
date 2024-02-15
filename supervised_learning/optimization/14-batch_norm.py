9914dddba998fca5f2b75a1fd7c4614593b6a936
Jabulani-N <jabu.94@gmail.com>
2024-02-14 20:32:45 -0600

    task 14 created

---
+++ supervised_learning/optimization/14-batch_norm.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""module documentation
+this module creates a function.
+
+"""
+
+
+import tensorflow.compat.v1 as tf
+
+
+def create_batch_norm_layer(prev, n, activation):
+    """placeholder documentation"""
+    pass
