d538e8a00e80111f63281146ae3eb33ae5368ef8
Jabulani-N <jabu.94@gmail.com>
2024-02-14 18:22:32 -0600

    readme: noted taht it is not complete;task 11 style improvements

--- supervised_learning/optimization/11-learning_rate_decay.py
+++ supervised_learning/optimization/11-learning_rate_decay.py
@@ -10,10 +10,11 @@ import numpy as np
 
 def learning_rate_decay(alpha, decay_rate, global_step, decay_step):
     """
-    alpha is the original learning rate
-    decay_rate is the weight used to determine the rate at which alpha will decay
-    global_step is the number of passes of gradient descent that have elapsed
-    decay_step is the number of passes of gradient descent that should occur before alpha is decayed further
+    alpha = original learning rate
+    decay_rate = weight used to determine the rate at which alpha will decay
+    global_step = number of gradient descent passes elapsed
+    decay_step = number of gradient descent passes that occur
+        before alpha is decayed further
     the learning rate decay should occur in a stepwise fashion
     Returns: the updated value for alpha
     """
--- supervised_learning/optimization/README.md
+++ supervised_learning/optimization/README.md
@@ -2,6 +2,8 @@
 
 This direcetory's parameters are hyper.
 
+Due to multiple compatibility errors and a time crunch, this directory was not able to be written in a manner comprehensively enough for one to learn the entire project. My apologies.
+
 # Task 0 Normalization Constants
 
 Though standard deviation and mean are values we calculated in the [math](https://github.com/Jabulani-N/holbertonschool-machine_learning/tree/main/math/probability) course, because numpy is availible this course, we can directly have it calculate [mean](https://numpy.org/doc/stable/reference/generated/numpy.mean.html) and [standard deviation](https://numpy.org/doc/stable/reference/generated/numpy.std.html.
