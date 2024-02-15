1ef7577758f056a96a49fcafad93392b806ec8a1
Jabulani-N <jabu.94@gmail.com>
2024-02-14 18:51:29 -0600

    task 12 created

---
+++ supervised_learning/optimization/12-learning_rate_decay.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""module documentation
+this module creates a function.
+
+"""
+
+
+import  tensorflow.compat.v1 as tf
+
+
+def learning_rate_decay(alpha, decay_rate, global_step, decay_step):
+    """placeholder documentqation"""
+    pass
