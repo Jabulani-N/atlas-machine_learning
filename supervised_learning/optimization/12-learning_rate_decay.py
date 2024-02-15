72173c2a62474123ed862c77bdc08ec18ecd91b4
Jabulani-N <jabu.94@gmail.com>
2024-02-14 19:01:05 -0600

    task 12 populated via resource in readme

--- supervised_learning/optimization/12-learning_rate_decay.py
+++ supervised_learning/optimization/12-learning_rate_decay.py
@@ -10,4 +10,8 @@ import tensorflow.compat.v1 as tf
 
 def learning_rate_decay(alpha, decay_rate, global_step, decay_step):
     """placeholder documentation"""
-    pass
+    rate_op = tf.train.inverse_time_decay(
+        alpha, global_step,
+        decay_step, decay_rate,
+        staircase=True)
+    return rate_op
