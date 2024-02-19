2d7e35eb652633f0f0f1641d0fcb8fe8a6eda94a
Jabulani-N <jabu.94@gmail.com>
2024-02-19 17:08:11 -0600

    task 3 created

---
+++ supervised_learning/error_analysis/3-specificity.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""module documentation
+this module creates a function.
+"""
+
+
+import numpy as np
+
+
+def specificity(confusion):
+    """placeholder documentation"""
+    pass
