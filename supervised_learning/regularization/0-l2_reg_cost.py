112305de7d02bd7442579022b780db642ee99358
Jabulani-N <jabu.94@gmail.com>
2024-02-20 17:27:56 -0600

    created directory

---
+++ supervised_learning/regularization/0-l2_reg_cost.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""module documentation
+this module creates a function.
+"""
+
+
+import numpy as np
+import tensorflow.compat.v1 as tf
+
+
+def l2_reg_cost(cost, lambtha, weights, L, m):
+    """placeholder documentation"""
+    pass
