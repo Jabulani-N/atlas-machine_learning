27ac5d7d2bfeb9badf35bd6aca5e2fc779dffdf7
Jabulani-N <jabu.94@gmail.com>
2024-02-21 17:22:26 -0600

    task 1 completed documentation

--- supervised_learning/regularization/1-l2_reg_gradient_descent.py
+++ supervised_learning/regularization/1-l2_reg_gradient_descent.py
@@ -6,5 +6,18 @@ import numpy as np
 
 
 def l2_reg_gradient_descent(Y, weights, cache, alpha, lambtha, L):
-    """placeholder documentation"""
+    """
+    Y = one-hot numpy.ndarray
+        shape (classes, m)
+        contains the correct labels for the data
+    classes = number of classes
+    m = number of data points
+    weights = dictionary of the weights and biases of the neural network
+    cache = dictionary of the outputs of each layer of the neural network
+    alpha = learning rate
+    lambtha = L2 regularization parameter
+    L = number of layers of the network
+    use tanh activations on each layer
+        except the last, which uses a softmax activation
+    """
     pass
--- supervised_learning/regularization/Article Notes.md
+++ supervised_learning/regularization/Article Notes.md
@@ -2,7 +2,7 @@
 
 Regularization is used to prevent a prediction model from overfitting: becoming so extremely good at predicting a given data set that it becomes less useful when applied to others.
 
-L2 Regularization proportoinately alters the weights to prevent them from ever perfectly fitting a given dataset. 
+L2 Regularization proportoinately alters the weights to prevent them from ever perfectly fitting a given dataset.
 The basic idea is
 * `L2_cost = sum of (all individual weights squared)`
 * `L2_regularization = (lambtha / (2 * m)) * l2_cost`
