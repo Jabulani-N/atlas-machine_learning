261b0625a1ae2ba21a5d773e67ef83ca301e7410
Jabulani-N <jabu.94@gmail.com>
2024-02-21 15:21:24 -0600

    task 0 passes tests; readme: explained process for task 0

--- supervised_learning/regularization/0-l2_reg_cost.py
+++ supervised_learning/regularization/0-l2_reg_cost.py
@@ -9,5 +9,16 @@ import tensorflow.compat.v1 as tf
 
 
 def l2_reg_cost(cost, lambtha, weights, L, m):
-    """placeholder documentation"""
-    pass
+    """
+    cost = cost of network without L2 regularization
+    lambtha = regularization parameter
+    weights = dictionary of weights and biases (numpy.ndarrays) of neural network
+    L = number of layers in neural network
+    m = number of data points used
+    """
+    l2_cost = 0
+    for layer in range(1, L + 1):
+        l2_cost += np.sum(np.square(weights["W" + str(layer)]))
+    l2_regularization = (lambtha / (2 * m)) * l2_cost
+    new_cost = cost + l2_regularization
+    return new_cost
--- supervised_learning/regularization/README.md
+++ supervised_learning/regularization/README.md
@@ -1,1 +1,9 @@
 # Regularization
+
+
+## Task 0 L2 Regularization Cost
+
+Basic idea:
+1. `L2_cost = sum of (all individual weights squared)`
+2. `l2_regularization = (lambtha / (2 * m)) * l2_cost`
+3. `new_cost = cost + l2_regularization`
