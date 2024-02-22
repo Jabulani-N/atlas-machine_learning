dadca9e039259882291802a9dc0a959a05835871
Jabulani-N <jabu.94@gmail.com>
2024-02-21 18:11:04 -0600

    task 1: passes test file

--- supervised_learning/regularization/1-l2_reg_gradient_descent.py
+++ supervised_learning/regularization/1-l2_reg_gradient_descent.py
@@ -22,6 +22,18 @@ def l2_reg_gradient_descent(Y, weights, cache, alpha, lambtha, L):
     """
     classes, m = np.shape(Y)
     dz = cache["A" + str(L)] - Y
-    # dz = cost of last activation slot
-    # Remember from the previous weeks that cache[A0] is input
-    #   so each layer is naturally enumerated (layer 1 = cache[A1])
+    # backpropagation
+    for layer_num in range(L, 0, -1):
+        A_prev = cache["A" + str(layer_num - 1)]
+        # we don't have to initialize this because we're starting at the end
+        #   and never going to alter position 0, which is input
+        dW = (1 / m) * np.dot(dz, A_prev.T) +\
+            (lambtha / m) * weights["W" + str(layer_num)]
+        # same as how we used to do backprop, but adding L2 regularization
+        db = (1 / m) * np.sum(dz, axis=1, keepdims=True)
+        dA_prev = np.dot(weights["W" + str(layer_num)].T, dz)
+        weights["W" + str(layer_num)] -= alpha * dW
+        weights["b" + str(layer_num)] -= alpha * db
+        if layer_num > 1:
+            dz = dA_prev * (1 - np.power(A_prev, 2))
+    return weights
