ac2c04e2514c5367e35998df5b8190e6dbea3596
Jabulani-N <jabu.94@gmail.com>
2024-02-21 20:34:08 -0600

    task 2 style improvement

--- supervised_learning/regularization/2-l2_reg_cost.py
+++ supervised_learning/regularization/2-l2_reg_cost.py
@@ -9,6 +9,7 @@ def l2_reg_cost(cost):
     """placeholder documentation"""
     L2_regularizer = tf.keras.regularizers.L2()
     all_weights = [v for v in tf.trainable_variables() if "weights" in v.name]
-    l2_penalty = tf.contrib.layers.apply_regularization(L2_regularizer, all_weights)
+    l2_penalty = tf.contrib.layers.apply_regularization(L2_regularizer,
+                                                        all_weights)
     negative_penalty = cost - l2_penalty
     return negative_penalty
