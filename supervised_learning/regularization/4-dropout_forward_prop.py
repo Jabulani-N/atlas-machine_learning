7c0a674382184b12b6a7bf84d751cf852f5f2734
Jabulani-N <jabu.94@gmail.com>
2024-02-21 21:12:43 -0600

    task 4 begun

--- supervised_learning/regularization/4-dropout_forward_prop.py
+++ supervised_learning/regularization/4-dropout_forward_prop.py
@@ -6,5 +6,15 @@ import numpy as np
 
 
 def dropout_forward_prop(X, weights, L, keep_prob):
-    """placeholder documentation"""
-    pass
+    """
+    X = numpy.ndarray of input data
+        shape (nx, m)
+            nx is the number of input features
+            m is the number of data points
+    weights = dictionary of the weights and biases of the neural network
+    L = number of layers in the network
+    keep_prob = probability that a node will be kept
+    """
+    cache = {}
+    Activation = X
+    dropout_masks = {}
