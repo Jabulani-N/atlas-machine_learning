e46b7bd1c4958ab62de2e7c24f10a8569cddf703
Jabulani-N <jabu.94@gmail.com>
2024-02-22 00:02:11 -0600

    task5,6 created placeholders

---
+++ supervised_learning/regularization/5-dropout_gradient_descent.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""this module creates a function."""
+
+
+import numpy as np
+
+
+def dropout_gradient_descent(Y, weights, cache, alpha, keep_prob, L):
+    """placeholder"""
+    pass
---
+++ supervised_learning/regularization/6-dropout_create_layer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""this module creates a function."""
+
+
+import numpy as np
+
+
+def dropout_create_layer(prev, n, activation, keep_prob):
+    """placeholder"""
+    pass
