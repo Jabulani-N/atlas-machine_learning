9e8c7546dc5b3d9b65e7c1fc9c2e0e5a05f53167
Jabulani-N <jabu.94@gmail.com>
2024-02-21 20:38:41 -0600

    task 4 created

---
+++ supervised_learning/regularization/4-dropout_forward_prop.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""this module creates a function."""
+
+
+import tensorflow.compat.v1 as tf
+
+
+def dropout_forward_prop(X, weights, L, keep_prob):
+    """placeholder documentation"""
+    pass
