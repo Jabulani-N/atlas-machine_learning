d6e38fe9ca480695f30658703252ea39417e1f06
Jabulani-N <jabu.94@gmail.com>
2024-02-23 18:42:22 -0600

    task 0 populated

--- supervised_learning/keras/0-sequential.py
+++ supervised_learning/keras/0-sequential.py
@@ -7,4 +7,14 @@ import tensorflow.keras as K
 
 def build_model(nx, layers, activations, lambtha, keep_prob):
     """placeholder documentation"""
-    pass
+    model = K.models.Sequential()
+
+    for i in range(len(layers)):
+        if i == 0:
+            model.add(K.layers.Dense(layers[i], input_dim=nx, activation=activations[i], kernel_regularizer=K.regularizers.l2(lambtha)))
+        else:
+            model.add(K.layers.Dense(layers[i], activation=activations[i], kernel_regularizer=K.regularizers.l2(lambtha)))
+        if i < len(layers) - 1:
+            model.add(K.layers.Dropout(1 - keep_prob))
+
+    return model
