cc2e2644a9ebbd19a55b1dca4281d353669bec8e
Jabulani-N <jabu.94@gmail.com>
2024-02-23 18:43:40 -0600

    task 0 style improvementes

--- supervised_learning/keras/0-sequential.py
+++ supervised_learning/keras/0-sequential.py
@@ -12,12 +12,12 @@ def build_model(nx, layers, activations, lambtha, keep_prob):
     for i in range(len(layers)):
         if i == 0:
             model.add(K.layers.Dense(layers[i],
-            input_dim=nx, activation=activations[i],
-            kernel_regularizer=K.regularizers.l2(lambtha)))
+                      input_dim=nx, activation=activations[i],
+                      kernel_regularizer=K.regularizers.l2(lambtha)))
         else:
             model.add(K.layers.Dense(layers[i],
-            activation=activations[i],
-            kernel_regularizer=K.regularizers.l2(lambtha)))
+                      activation=activations[i],
+                      kernel_regularizer=K.regularizers.l2(lambtha)))
         if i < len(layers) - 1:
             model.add(K.layers.Dropout(1 - keep_prob))
 
