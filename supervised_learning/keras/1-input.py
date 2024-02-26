c1b7a9e2fa5adca3271cac83969cccc84d73aaae
Jabulani-N <jabu.94@gmail.com>
2024-02-26 15:40:59 -0600

    task 1 created

---
+++ supervised_learning/keras/1-input.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""this module creates a function."""
+
+
+import tensorflow.keras as K
+
+
+def build_model(nx, layers, activations, lambtha, keep_prob):
+    """placeholder documentation"""
+    pass
