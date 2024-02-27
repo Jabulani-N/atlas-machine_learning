a7d0b085aacf9d6fa494564fefaff7f090b6cbc1
Jabulani-N <jabu.94@gmail.com>
2024-02-27 00:46:25 -0600

    task 2 populated

--- supervised_learning/keras/2-optimize.py
+++ supervised_learning/keras/2-optimize.py
@@ -6,5 +6,15 @@ import tensorflow.keras as K
 
 
 def optimize_model(network, alpha, beta1, beta2):
-    """placeholder documentation"""
-    pass
+    """network is the model to optimize
+    alpha is the learning rate
+    beta1 is the first Adam optimization parameter
+    beta2 is the second Adam optimization parameter
+
+    crossentropy loss
+    accuracy metrics
+    """
+    optimizer = K.optimizers.Adam(learning_rate=alpha,
+                            beta_1=beta1,
+                            beta_2=beta2)
+    network.compile(optimizer=optimizer, loss="crossentropy", metrics="accuracy")
