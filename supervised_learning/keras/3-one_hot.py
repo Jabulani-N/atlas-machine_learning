3151ee3b8de15f116b5096d4b1f43d91cedce510
Jabulani-N <jabu.94@gmail.com>
2024-02-27 17:31:33 -0600

    task 3 created zeros matrix within import limitations

--- supervised_learning/keras/3-one_hot.py
+++ supervised_learning/keras/3-one_hot.py
@@ -6,5 +6,14 @@ import tensorflow.keras as K
 
 
 def one_hot(labels, classes=None):
-    """placeholder documentation"""
-    pass
+    """
+    converts a list of labels (int)
+    into a one-hot matrix
+    """
+    hottie = []
+    class_count = len(labels)
+    # initialize one-hot matrix with 0s
+    for dim0 in range(class_count):
+        hottie.append([])
+        for dim1 in range(class_count):
+            hottie[dim0].append(0)
