e56d618ef235880ae9d096f942a9f65abea3619f
Jabulani-N <jabu.94@gmail.com>
2024-02-27 17:34:11 -0600

    task 3 test succeeded locally

--- supervised_learning/keras/3-one_hot.py
+++ supervised_learning/keras/3-one_hot.py
@@ -17,3 +17,5 @@ def one_hot(labels, classes=None):
         hottie.append([])
         for dim1 in range(class_count):
             hottie[dim0].append(0)
+        hottie[dim0][labels[dim0]] = 1
+    return hottie
