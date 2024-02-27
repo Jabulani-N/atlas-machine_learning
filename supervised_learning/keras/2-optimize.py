75903e01947c550d7e72d10f168681b4c75915d6
Jabulani-N <jabu.94@gmail.com>
2024-02-27 00:47:11 -0600

    task 2 style improvement

--- supervised_learning/keras/2-optimize.py
+++ supervised_learning/keras/2-optimize.py
@@ -17,4 +17,6 @@ def optimize_model(network, alpha, beta1, beta2):
     optimizer = K.optimizers.Adam(learning_rate=alpha,
                                   beta_1=beta1,
                                   beta_2=beta2)
-    network.compile(optimizer=optimizer, loss="crossentropy", metrics="accuracy")
+    network.compile(optimizer=optimizer,
+                    loss="crossentropy",
+                    metrics="accuracy")
