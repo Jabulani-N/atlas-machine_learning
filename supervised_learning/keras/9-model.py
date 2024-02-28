66f16f5097b19a3ca3c92f5b346c49c2c780a7d7
Jabulani-N <jabu.94@gmail.com>
2024-02-28 01:19:53 -0600

    task 9 created with save model function

---
+++ supervised_learning/keras/9-model.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""this module creates 2 functions."""
+
+
+import tensorflow.keras as K
+
+
+def save_model(network, filename):
+    """saves entire model"""
+    network.save(filename)
+
+
+def load_model(filename):
+    """loads a model"""
+    pass
