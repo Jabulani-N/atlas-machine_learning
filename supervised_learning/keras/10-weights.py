90daebbc5c03200362c44816f18f24626304086f
Jabulani-N <jabu.94@gmail.com>
2024-02-28 01:36:38 -0600

    task 10 created

---
+++ supervised_learning/keras/10-weights.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""this module creates 2 functions."""
+
+
+import tensorflow.keras as K
+
+
+def save_weights(network, filename, save_format='h5'):
+    """
+    saves a model’s weights
+    """
+    pass
+
+def load_weights(network, filename):
+    """loads a model’s weights"""
+    pass
