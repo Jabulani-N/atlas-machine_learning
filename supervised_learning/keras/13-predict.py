66bce49d363b162e18aafc015149b9a4ef5c5bdd
Jabulani-N <jabu.94@gmail.com>
2024-02-28 21:41:45 -0600

    task 13 created

---
+++ supervised_learning/keras/13-predict.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""this module creates a function."""
+
+
+import tensorflow.keras as K
+
+
+def predict(network, data, verbose=False):
+    """makes a prediction using a neural network"""
+    pass
