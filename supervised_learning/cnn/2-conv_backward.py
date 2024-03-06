f3339ce0646d06b30d948c38aca86c44d1a1de2e
Jabulani-N <jabu.94@gmail.com>
2024-03-05 23:20:44 -0600

    task 2 created

---
+++ supervised_learning/cnn/2-conv_backward.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""placeholder documentation"""
+
+
+import numpy as np
+
+
+def conv_backward(dZ, A_prev, W, b, padding="same", stride=(1, 1)):
+    """
+    performs back propagation
+        over a convolutional layer of a neural network
+    """
+    pass
