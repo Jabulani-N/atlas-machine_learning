4ccb3ed39ad0bbfefd33a0b53770dad9b286df03
Jabulani-N <jabu.94@gmail.com>
2024-03-06 20:53:52 -0600

    task 4, 5 files created

---
+++ supervised_learning/cnn/4-lenet5.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""placeholder documentation"""
+
+
+import tensorflow.compat.v1 as tf
+
+
+def lenet5(x, y):
+    """placeholder documentation"""
+    pass
---
+++ supervised_learning/cnn/5-lenet5.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""placeholder documentation"""
+
+
+import tensorflow.keras as K
+
+
+def lenet5(X):
+    """placeholder documentation"""
+    pass
