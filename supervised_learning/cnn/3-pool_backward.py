a03f8ff2abe425fe51f3850af76591023c7f2632
Jabulani-N <jabu.94@gmail.com>
2024-03-06 19:46:05 -0600

    task 3 has an output locally, but not what is expected

--- supervised_learning/cnn/3-pool_backward.py
+++ supervised_learning/cnn/3-pool_backward.py
@@ -10,4 +10,29 @@ def pool_backward(dA, A_prev, kernel_shape, stride=(1, 1), mode='max'):
     performs back propagation
         over a pooling layer of a neural network
     """
-    pass
+    m, h_new, w_new, c = dA.shape
+    m, h_prev, w_prev, c = A_prev.shape
+    kh, kw = kernel_shape
+    sh, sw = stride
+
+    dA_prev = np.zeros(A_prev.shape)
+
+    for i in range(m):
+        for h in range(h_new):
+            for w in range(w_new):
+                for c in range(c):
+                    vert_start = h * sh
+                    vert_end = vert_start + kh
+                    horiz_start = w * sw
+                    horiz_end = horiz_start + kw
+
+                    if mode == 'max':
+                        a_prev_slice = A_prev[i, vert_start:vert_end, horiz_start:horiz_end, c]
+                        mask = (a_prev_slice == np.max(a_prev_slice))
+                    elif mode == 'avg':
+                        mask = np.ones((kh, kw)) / (kh * kw)
+
+                    da = dA[i, h, w, c]
+                    dA_prev[i, vert_start:vert_end, horiz_start:horiz_end, c] += mask * da
+
+    return dA_prev
