e1c4d5896b30cff81739954970660b3b6c31bbce
Jabulani-N <jabu.94@gmail.com>
2024-03-12 00:01:56 -0500

    task1 created

---
+++ supervised_learning/deep_cnns/1-inception_network.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""this module creates a function."""
+
+
+import tensorflow.keras as K
+
+
+def inception_network():
+    """
+    builds the inception network as described in
+        https://arxiv.org/pdf/1409.4842.pdf
+    You can assume  input data will have shape (224, 224, 3)
+    All convolutions inside and outside inception block
+        use ReLU activation
+    you may inception_block = __import__('0-inception_block').inception_block
+    Returns: the keras model
+    """
+    pass
