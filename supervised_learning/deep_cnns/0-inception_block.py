d0677bc500a7fd19f4b303937068861610b73a06
Jabulani-N <jabu.94@gmail.com>
2024-03-11 21:04:50 -0500

    task0 minor cleanup

--- supervised_learning/deep_cnns/0-inception_block.py
+++ supervised_learning/deep_cnns/0-inception_block.py
@@ -8,19 +8,19 @@ def inception_block(A_prev, filters):
     """
     builds an inception block as described in
         https://arxiv.org/pdf/1409.4842.pdf
-    A_prev = the output from the previous layer
+    A_prev = output from previous layer
     filters = a tuple or list
         containing [F1, F3R, F3,F5R, F5, FPP]:
-            F1 is the number of filters in the 1x1 convolution
-            F3R = the number of filters in the 1x1 convolution before the 3x3 convolution
-            F3 = the number of filters in the 3x3 convolution
-            F5R = the number of filters in the 1x1 convolution before the 5x5 convolution
-            F5 = the number of filters in the 5x5 convolution
-            FPP = the number of filters in the 1x1 convolution after the max pooling
-    All convolutions inside the inception block should use a rectified linear activation (ReLU)
-    Returns: the concatenated output of the inception block
+            F1 = number of filters in 1x1 convolution
+            F3R = number of filters in 1x1 convolution before the 3x3 convolution
+            F3 = number of filters in 3x3 convolution
+            F5R = number of filters in 1x1 convolution before the 5x5 convolution
+            F5 = number of filters in 5x5 convolution
+            FPP = number of filters in 1x1 convolution after the max pooling
+    All convolutions inside inception block should use a rectified linear activation (ReLU)
+    Returns: concatenated output of inception block
     """
     F1, F3R, F3,F5R, F5, FPP = filters
     # extract parameters from list filters via direct assignment
 
-    convf1 = K.layers.Conv2D(pass)
+    convf1 = K.layers.Conv2D()
