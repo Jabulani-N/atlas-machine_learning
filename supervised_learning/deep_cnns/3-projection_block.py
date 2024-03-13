0dff77553ee19464e720eb23c793bf0cec553b27
Jabulani-N <jabu.94@gmail.com>
2024-03-13 18:12:30 -0500

    task 3 style correctoins

--- supervised_learning/deep_cnns/3-projection_block.py
+++ supervised_learning/deep_cnns/3-projection_block.py
@@ -12,18 +12,26 @@ def projection_block(A_prev, filters, s=2):
     """
     F11, F3, F12 = filters
 
-    X_shortcut = K.layers.Conv2D(filters=F12, kernel_size=(1, 1), strides=(s, s), padding='valid', kernel_initializer='he_normal')(A_prev)
+    X_shortcut = K.layers.Conv2D(filters=F12, kernel_size=(1, 1), strides=(s, s),
+                                 padding='valid',
+                                 kernel_initializer='he_normal')(A_prev)
     X_shortcut = K.layers.BatchNormalization(axis=3)(X_shortcut)
 
-    X = K.layers.Conv2D(filters=F11, kernel_size=(1, 1), strides=(s, s), padding='valid', kernel_initializer='he_normal')(A_prev)
+    X = K.layers.Conv2D(filters=F11, kernel_size=(1, 1), strides=(s, s),
+                        padding='valid',
+                        kernel_initializer='he_normal')(A_prev)
     X = K.layers.BatchNormalization(axis=3)(X)
     X = K.layers.Activation('relu')(X)
 
-    X = K.layers.Conv2D(filters=F3, kernel_size=(3, 3), strides=(1, 1), padding='same', kernel_initializer='he_normal')(X)
+    X = K.layers.Conv2D(filters=F3, kernel_size=(3, 3), strides=(1, 1),
+                        padding='same',
+                        kernel_initializer='he_normal')(X)
     X = K.layers.BatchNormalization(axis=3)(X)
     X = K.layers.Activation('relu')(X)
 
-    X = K.layers.Conv2D(filters=F12, kernel_size=(1, 1), strides=(1, 1), padding='valid', kernel_initializer='he_normal')(X)
+    X = K.layers.Conv2D(filters=F12, kernel_size=(1, 1), strides=(1, 1),
+                        padding='valid',
+                        kernel_initializer='he_normal')(X)
     X = K.layers.BatchNormalization(axis=3)(X)
 
     X = K.layers.Add()([X, X_shortcut])
