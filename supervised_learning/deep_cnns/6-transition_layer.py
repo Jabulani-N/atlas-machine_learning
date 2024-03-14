55caaa3dc782e844a6f039eac7c499d7710dfd9c
Jabulani-N <jabu.94@gmail.com>
2024-03-13 23:21:45 -0500

    task 6 style improvement

--- supervised_learning/deep_cnns/6-transition_layer.py
+++ supervised_learning/deep_cnns/6-transition_layer.py
@@ -14,8 +14,12 @@ def transition_layer(X, nb_filters, compression):
     compression is the compression factor for the transition layer
     Your code should implement compression as used in DenseNet-C
     All weights should use he normal initialization
-    All convolutions should be preceded by Batch Normalization and a rectified linear activation (ReLU), respectively
-    Returns: The output of the transition layer and the number of filters within the output, respectively
+    All convolutions preceded by
+        Batch Normalization
+        relu
+    Returns:
+    output of the transition layer
+    number of filters within the output
     """
     X = K.layers.BatchNormalization(axis=3)(X)
     X = K.layers.Activation('relu')(X)
