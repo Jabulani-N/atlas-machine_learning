e1ca05ef7c75c02436bad8caf36703955f1d8508
Jabulani-N <jabu.94@gmail.com>
2024-03-13 22:33:10 -0500

    task 5 minor update

--- supervised_learning/deep_cnns/5-dense_block.py
+++ supervised_learning/deep_cnns/5-dense_block.py
@@ -23,22 +23,21 @@ def dense_block(X, nb_filters, growth_rate, layers):
         concatenated output of each layer within Dense Block
         number of filters within concatenated outputs
     """
-    concatenated_outputs = [X]
-    for _ in range(layers):
-        X = K.layers.BatchNormalization(axis=3)(X)
-        X = K.layers.Activation('relu')(X)
-        X = K.layers.Conv2D(filters=4 * growth_rate,
-                            kernel_size=(1, 1), strides=(1, 1),
-                            kernel_initializer='he_normal')(X)
 
-        X = K.layers.BatchNormalization(axis=3)(X)
-        X = K.layers.Activation('relu')(X)
-        X = K.layers.Conv2D(filters=growth_rate,
+    for layerNum in range(layers):
+        out = K.layers.BatchNormalization(X)
+        out = K.layers.Activation('relu')(out)
+        out = K.layers.Conv2D(filters=4 * growth_rate,
+                            kernel_size=(1, 1), strides=(1, 1),
+                            kernel_initializer='he_normal')(out)
+
+        out = K.layers.BatchNormalization(axis=3)(out)
+        out = K.layers.Activation('relu')(out)
+        out = K.layers.Conv2D(filters=growth_rate,
                             kernel_size=(3, 3), strides=(1, 1),
                             padding='same', kernel_initializer='he_normal')(X)
 
-        concatenated_outputs.append(X)
-        X = K.layers.Concatenate(axis=3)([concatenated_outputs])
+        X = K.layers.Concatenate([X, out])
 
         nb_filters += growth_rate
 
