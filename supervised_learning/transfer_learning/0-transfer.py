011f8655667e18be6519b8d82769187c57c0f9f5
Jabulani-N <jabu.94@gmail.com>
2024-03-20 20:12:31 -0500

    task0: begane coding in google colab, and transferring bits that seem to work to the task 0 file

---
+++ supervised_learning/transfer_learning/0-transfer.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""placeholder documentation"""
+
+
+import tensorflow.keras as K
+
+
+def preprocess_data(X, Y):
+    X_p = K.applications.efficientnet.preprocess_input(X)
+    Y_p = K.utils.to_categorical(Y, 10)
+    return X_p, Y_p
+# the above preprocess_data() seems to work
+
+# Load the CIFAR10 dataset
+(x_train, y_train), (x_test, y_test) = K.datasets.cifar10.load_data()
+
+# Preprocess the data using the preprocess_data function
+x_train, y_train = preprocess_data(x_train, y_train)
+x_test, y_test = preprocess_data(x_test, y_test)
