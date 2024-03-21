fbffedd57f6449b7dbaa3d14b355d329dd47b6fc
Jabulani-N <jabu.94@gmail.com>
2024-03-20 20:54:51 -0500

    task0: made it so it only runs if main

--- supervised_learning/transfer_learning/0-transfer.py
+++ supervised_learning/transfer_learning/0-transfer.py
@@ -8,17 +8,27 @@ import tensorflow.keras as K
 def preprocess_data(X, Y):
     """
     runs preprocessing on input dataset
-    recieves a keras.datasets.datasetName.load_data()
+    recieves a keras.datasets.datasetName.load_data()[i]
         which returns two objects when run
     """
     X_p = K.applications.efficientnet.preprocess_input(X)
     Y_p = K.utils.to_categorical(Y, 10)
     return X_p, Y_p
 # the above preprocess_data() seems to work
+def make_model():
+    """
+    makes an EfficientNetB7 model
+    retrains it for the cifar10 dataset
+    """
+    # Load the CIFAR10 dataset
+    (x_train, y_train), (x_test, y_test) = K.datasets.cifar10.load_data()
 
-# Load the CIFAR10 dataset
-(x_train, y_train), (x_test, y_test) = K.datasets.cifar10.load_data()
+    # Preprocess the data using the preprocess_data function
+    x_train, y_train = preprocess_data(x_train, y_train)
+    x_test, y_test = preprocess_data(x_test, y_test)
 
-# Preprocess the data using the preprocess_data function
-x_train, y_train = preprocess_data(x_train, y_train)
-x_test, y_test = preprocess_data(x_test, y_test)
+    # Create the EfficientNetB7 model
+    model = K.applications.EfficientNetB7(weights='imagenet', include_top=True)
+
+if __name__ == '__main__':
+    madeModel = make_model()
