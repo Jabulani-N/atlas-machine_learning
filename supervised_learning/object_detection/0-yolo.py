83ce894917f75ee5f2246c0dcf634ceb5d7cf18f
Jabulani-N <jabu.94@gmail.com>
2024-04-03 17:21:44 -0500

    task0 reworked imports and variable names

--- supervised_learning/object_detection/0-yolo.py
+++ supervised_learning/object_detection/0-yolo.py
@@ -6,7 +6,7 @@ There are no import restrictions
 
 
 import numpy as np
-from tensorflow.keras.models import load_model
+import tensorflow.keras as K
 
 
 class Yolo:
@@ -33,10 +33,11 @@ class Yolo:
         anchors: the anchor boxes
         """
         # Load the Darknet Keras model
-        self.model = load_model(model_path)
-        with open(classes_path, 'r') as file:
+        self.model = K.models.load_model(model_path)
+        with open(classes_path, 'r') as classes_file:
             # Read and store the list of class names
-            self.class_names = [line.strip() for line in file.readlines()]
+            self.class_names = [line.strip()
+                                for line in classes_file.readlines()]
         # Assign the box score threshold for the initial filtering step
         self.class_t = class_t
         # Assign the IOU threshold for non-max suppression
