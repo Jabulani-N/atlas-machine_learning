39af2dec26ff52093cb696b4d22768916a9075e2
Jabulani-N <jabu.94@gmail.com>
2024-04-17 01:18:24 -0500

    task 6 fixed put text the same way as cv2.rectangle was fixed before: specifying int cast. Task 2,3 placeholders created

---
+++ supervised_learning/object_detection/2-yolo.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+"""
+placeholder module
+This module creates a class Yolo
+There are no import restrictions
+"""
+
+
+import numpy as np
+import tensorflow.keras as K
+
+
+class Yolo:
+    """
+    model_path = path to where a Darknet Keras model is stored
+    classes_path = path to list of class names used for Darknet model
+        listed in order of index
+    class_t = float representing box score threshold for initial filtering
+    nms_t = float representing the IOU threshold for non-max suppression
+    anchors is a numpy.ndarray
+        shape (outputs, anchor_boxes, 2)
+            outputs = number of outputs (predictions) made by the Darknet model
+        containins all  anchor boxes:
+            anchor_boxes = number of anchor boxes used for each prediction
+        2 => [anchor_box_width, anchor_box_height]
+    """
+    def __init__(self, model_path, classes_path, class_t, nms_t, anchors):
+        """
+        Public instance attributes:
+        model: the Darknet Keras model
+        class_names: a list of the class names for the model
+        class_t: the box score threshold for the initial filtering step
+        nms_t: the IOU threshold for non-max suppression
+        anchors: the anchor boxes
+        """
+        # Assign the box score threshold for the initial filtering step
+        self.class_t = class_t
+        # Assign the IOU threshold for non-max suppression
+        self.nms_t = nms_t
+        # Assign the anchor boxes
+        self.anchors = anchors
+
+        # Load the Darknet Keras model
+        self.model = K.models.load_model(model_path)
+        with open(classes_path, 'r') as classes_file:
+            # Read and store the list of class names
+            self.class_names = [line.strip()
+                                for line in classes_file.readlines()]
