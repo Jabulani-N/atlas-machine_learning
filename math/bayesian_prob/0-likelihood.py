a9c7e9f6578aebe26182759b2d7d829d97814417
Jabulani-N <jabu.94@gmail.com>
2024-05-09 10:14:12 -0500

    task 0 initialized

---
+++ math/bayesian_prob/0-likelihood.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""module documentation
+placeholder
+"""
+
+
+import numpy as np
+
+
+def likelihood(x, n, P):
+    """
+    calculates likelihood given various probabilities
+    x is the number of occurances
+    n is the total number of instances checked
+    P is a 1D numpy.ndarray
+        containing various hypothetical probabilities of occurance
+"""
+    if type(n) is not int or n <= 0:
+        raise ValueError("n must be a positive integer")
+    if type(x) is not int or x < 0:
+        raise ValueError("x must be an integer\
+                         that is greater than or equal to 0")
+    if x > n:
+        raise ValueError("x cannot be greater than n")
+    if type(P) is not np.ndarray or np.shape(P) != (1,):
+        raise TypeError("P must be a 1D numpy.ndarray")
+    for value in P:
+        if value < 0 or value >= 1:
+            raise ValueError("All values in P must be in the range [0, 1]")
---
+++ math/bayesian_prob/README.md
@@ -0,0 +1,6 @@
+# bayesian probability
+
+![Bayes Formula](https://miro.medium.com/v2/resize:fit:1400/format:webp/1*oKi6F9CNeCyhLajj_RRSoA.jpeg)
+
+  [source](https://towardsdatascience.com/what-is-bayes-rule-bb6598d8a2fd)
+
