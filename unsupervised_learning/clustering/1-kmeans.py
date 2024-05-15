c02614649ec66be03eca40ee5e2259c4453f2d53
Jabulani-N <jabu.94@gmail.com>
2024-05-15 02:16:57 -0500

    task 1 now finds closest centroids

--- unsupervised_learning/clustering/1-kmeans.py
+++ unsupervised_learning/clustering/1-kmeans.py
@@ -7,10 +7,25 @@ import numpy as np
 
 def kmeans(X, k, iterations=1000):
     """runs iteratsions iterations of k-means clustering"""
-    centroid = initialize(X, k)
-    if centroid is None or\
+    centroids = initialize(X, k)
+    if centroids is None or\
        iterations <= 0:
         return None, None
+    # print("sent centroids", centroids)
+    # print("sent X", X)
+    # note position 0 is first centroid at centroids[0]
+    closest = find_closest_centroid(centroids, X)
+    # print("decided closest centroids are:", closest)
+    return None, None
+
+
+def find_closest_centroid(centroids, X):
+    # Compute Euclidean distance btwn each data point and  centroid
+    distances = np.sqrt(np.sum((X[:, np.newaxis] - centroids) ** 2,
+                               axis=2))
+    # Find the index of the closest centroid for each data point
+    closest_centroid_indices = np.argmin(distances, axis=1)
+    return closest_centroid_indices
 
 
 def initialize(X, k):
