ae719106ea0422b3be9108095326697a5ade2806
Jabulani-N <jabu.94@gmail.com>
2024-05-23 00:05:50 -0500

    task 8 minor style improvement

--- unsupervised_learning/clustering/8-EM.py
+++ unsupervised_learning/clustering/8-EM.py
@@ -7,6 +7,7 @@ initialize = __import__('4-initialize').initialize
 expectation = __import__('6-expectation').expectation
 maximization = __import__('7-maximization').maximization
 
+
 def expectation_maximization(X, k, iterations=1000,
                              tol=1e-5, verbose=False):
     """expectation maximization of GMM"""
