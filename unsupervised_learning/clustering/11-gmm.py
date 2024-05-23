398e3994c307fc42bbd82a6a274da11fe76d4ae7
Jabulani-N <jabu.94@gmail.com>
2024-05-23 00:02:01 -0500

    task 11 in checker

--- unsupervised_learning/clustering/11-gmm.py
+++ unsupervised_learning/clustering/11-gmm.py
@@ -2,9 +2,25 @@
 """task 11"""
 
 
-import sklearn.cluster
+import sklearn.mixture
 
 
 def gmm(X, k):
     """calculate gmm of dataset"""
-    pass
+    gaussian_mix = sklearn.mixture.GaussianMixture
+    mix = gaussian_mix(n_components=k)
+    mix.fit(X)
+    pi = mix.weights_
+    m = mix.means_
+    S = mix.covariances_
+    clss = mix.predict(X)
+    kmin = 1
+    kmax = k
+    bic = []
+    for i in range(kmin, kmax + 1):
+        mix = gaussian_mix(n_components=i)
+        mix.fit(X)
+        bic.append(mix.bic(X))
+    bic = np.array(bic)
+
+    return pi, m, S, clss, bic
