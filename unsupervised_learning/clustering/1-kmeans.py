18f720caf2adefc1b0b795ab5c863ab26a3d2b6a
Jabulani-N <jabu.94@gmail.com>
2024-05-22 20:38:14 -0500

    task 1 tests successfully

--- unsupervised_learning/clustering/1-kmeans.py
+++ unsupervised_learning/clustering/1-kmeans.py
@@ -16,7 +16,34 @@ def kmeans(X, k, iterations=1000):
     # note position 0 is first centroid at centroids[0]
     closest = find_closest_centroid(centroids, X)
     # print("decided closest centroids are:", closest)
-    return None, None
+    # closest is a 1D list
+    centroids_after_iterating = np.copy(centroids)
+    old_centroid_pos = np.copy(centroids[0])
+    new_centroid_pos = np.copy(old_centroid_pos)
+    for iteration in range(iterations):
+        closest = find_closest_centroid(centroids, X)
+        for i in range(k):
+            cluster_points = X[closest == i]
+            if len(cluster_points) > 0:
+                centroids[i] = np.mean(cluster_points, axis=0)
+            else:
+                # Reinitialize the centroid if the cluster has no data points
+                minima = np.min(X, axis=0)
+                maxima = np.max(X, axis=0)
+                centroids[i] = np.random.uniform(minima, maxima)
+                # add to a count or something where if it equals the amount of total centroids
+                # then it instantly returns the final answer.
+                # we can do above part after we finish what an iteration looks like
+    return centroids, closest
+
+
+def relocate_centroid(X_trimmed, ):
+    """
+    relocates one centroid
+    by returning a single coordinate set
+    returns average of positions recieved
+    """
+    pass
 
 
 def find_closest_centroid(centroids, X):
