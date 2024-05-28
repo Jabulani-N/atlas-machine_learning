a99b115d360f97984261775eaa9254e21d4e05ea
Jabulani-N <jabu.94@gmail.com>
2024-05-27 20:56:29 -0500

    task 1 minor style improvements

--- unsupervised_learning/hmm/1-regular.py
+++ unsupervised_learning/hmm/1-regular.py
@@ -9,15 +9,20 @@ def regular(P):
     determines
     steady state probabilities of regular markov chain
     """
+    if not isinstance(P, np.ndarray):
+        return None  # Return None on failure
+    if P.shape[0] != P.shape[1]:
+        return None
+
     return steady_state_prop(P.transpose())
 
 
 def steady_state_prop(P):
     """a step used in steady state"""
     dim = P.shape[0]
-    q = (P-np.eye(dim))
+    q = (P - np.eye(dim))
     ones = np.ones(dim)
-    q = np.c_[q,ones]
+    q = np.c_[q, ones]
     QTQ = np.dot(q, q.T)
     bQT = np.ones(dim)
-    return np.linalg.solve(QTQ,bQT)
+    return np.linalg.solve(QTQ, bQT)
