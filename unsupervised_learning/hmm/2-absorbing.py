fcf72617b038c97467b1b240d73fa6a2eb2ce278
Jabulani-N <jabu.94@gmail.com>
2024-05-28 01:53:04 -0500

    task 2 now checks for absorbing being possible at all via excistance of a diagonally positoined '1'

--- unsupervised_learning/hmm/2-absorbing.py
+++ unsupervised_learning/hmm/2-absorbing.py
@@ -8,11 +8,22 @@ def absorbing(P):
     """determines if P is absorbinbg Markov chain
     P is 2D numpy.ndarray
     """
-    pass
+    if not isinstance(P, np.ndarray):
+        return None  # Return None on failure
+    if P.shape[0] != P.shape[1]:
+        return None
+    if np.any(np.less_equal(P, 0)):
+        return None
+    for subarray in P:
+        if np.any(np.less_equal(subarray, 0)):
+            return None
+    if diagonal_check(P, 1) is False:
+        return False
 
 def diagonal_check(P, target=1):
     """checks 2D array P
         for any instance of target
-            on a diagonal positoin
+            on a diagonal position
     """
-    pass
+    dia = np.diag(P)
+    return np.any(np.equal(dia, target))
