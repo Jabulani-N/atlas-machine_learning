1dbdcea9492d6f0cb4ea9685954edb328dd1bf7c
Jabulani-N <jabu.94@gmail.com>
2024-06-02 20:02:44 -0500

    directory created

---
+++ unsupervised_learning/hyperparameter_tuning/0-gp.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""task 0"""
+
+import numpy as np
+
+
+class GaussianProcess:
+    def __init__(self, X_init, Y_init, l=1, sigma_f=1):
+        """
+        documentation placeholder
+        """
+        pass
