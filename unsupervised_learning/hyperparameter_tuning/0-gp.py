b4cc07609da1c0640e4e329a794a15f9f498dd41
Jabulani-N <jabu.94@gmail.com>
2024-06-03 20:38:34 -0500

    task 0 reverted to previous version

--- unsupervised_learning/hyperparameter_tuning/0-gp.py
+++ unsupervised_learning/hyperparameter_tuning/0-gp.py
@@ -49,7 +49,5 @@ class GaussianProcess:
         # m, n = np.shape(X1)[0], np.shape(X2)[0]
         norm = np.square(np.linalg.norm(X1[None, :, :] - X2[:, None, :],
                                         axis=2).T)
-        norm = np.linalg.norm(X1[None, :, :] - X2[:, None, :], axis=2)
         kernel_matrix = np.exp(-norm / (2 * np.square(self.sigma_f)))
-        kernel_matrix = np.exp(-0.5 * np.square(norm) / np.square(self.sigma_f))
         return kernel_matrix
