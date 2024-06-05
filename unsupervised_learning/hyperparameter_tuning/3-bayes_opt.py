47f1558ddc491602973e168f7b6b88443c25bf35
Jabulani-N <jabu.94@gmail.com>
2024-06-04 19:12:22 -0500

    task 3 initialized

---
+++ unsupervised_learning/hyperparameter_tuning/3-bayes_opt.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""task 3"""
+
+import numpy as np
+
+class BayesianOptimization:
+    """performs Bayesian optimization on noiseless 1D Gaussian process"""
+    def __init__(self, X_init, Y_init, l=1, sigma_f=1):
+        """init doc placeholder"""
+        pass
