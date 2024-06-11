896615b141ba52dd7a46f4336ca9774e5871d5f6
Jabulani-N <jabu.94@gmail.com>
2024-06-11 02:45:58 -0500

    task 4 style improvement

--- unsupervised_learning/hyperparameter_tuning/4-bayes_opt.py
+++ unsupervised_learning/hyperparameter_tuning/4-bayes_opt.py
@@ -79,7 +79,9 @@ class BayesianOptimization:
             Z_for_X_next = imp / orig_variance
             EI = imp * norm.cdf(Z) + variance * norm.pdf(Z)
             # EI[variance == 0.0] = 0.0
-            EI_for_X_next = imp * norm.cdf(Z_for_X_next) + orig_variance * norm.pdf(Z_for_X_next)
-        X_next = self.X_s[np.where(EI_for_X_next == second_func(EI_for_X_next))[0][0]]
+            EI_for_X_next = imp * norm.cdf(Z_for_X_next) +\
+                orig_variance * norm.pdf(Z_for_X_next)
+        X_next = self.X_s[np.where(EI_for_X_next ==
+                                   second_func(EI_for_X_next))[0][0]]
         # X_next = self.X_s[argares(EI)]
         return X_next, EI[0]
