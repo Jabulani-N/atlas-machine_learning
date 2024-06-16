c2d299d15f22c443d72b0ea8846c5be1b24edd57
Jabulani-N <jabu.94@gmail.com>
2024-06-15 23:47:36 -0500

    direectory created. task 0 when tested in google colab, runs successfully. does not run locally

---
+++ unsupervised_learning/autoencoders/0-vanilla.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+"""task 0"""
+
+
+import tensorflow.keras as keras
+
+def autoencoder(input_dims, hidden_layers, latent_dims):
+    """creates an autoencoder
+
+    input_dims = integer containing the dimensions of the model input
+    hidden_layers = list containing the number of nodes for each hidden layer in the encoder, respectively
+    the hidden layers should be reversed for the decoder
+    latent_dims = integer containing the dimensions of the latent space representation
+    """
+    # return None
+    # we use this input one later, so it needs to be unique
+    encoder_input = keras.Input(shape=(input_dims,))
+    # must be defined this way
+    encoded = encoder_input
+    for nodes in hidden_layers:
+        encoded = keras.layers.Dense(nodes, activation='relu')(encoded)
+    encoder_output = keras.layers.Dense(latent_dims, activation='relu')(encoded)
+    encoder = keras.Model(encoder_input, encoder_output, name='encoder')
+
+    # as above, we need the unique input one
+    decoder_input = keras.Input(shape=(latent_dims,))
+    # must be defined this way
+    decoded = decoder_input
+    for nodes in reversed(hidden_layers):
+        decoded = keras.layers.Dense(nodes, activation='relu')(decoded)
+    decoder_output = keras.layers.Dense(input_dims, activation='sigmoid')(decoded)
+    decoder = keras.Model(decoder_input, decoder_output, name='decoder')
+
+    # Define the full autoencoder model
+    autoencoder_input = keras.Input(shape=(input_dims,))
+    encoded_output = encoder(autoencoder_input)
+    decoded_output = decoder(encoded_output)
+    auto = keras.Model(autoencoder_input, decoded_output, name='autoencoder')
+
+    # Compile the autoencoder model
+    auto.compile(optimizer='adam', loss='binary_crossentropy')
+
+    return encoder, decoder, auto
