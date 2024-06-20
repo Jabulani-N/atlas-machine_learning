f7adbbcad561f7abea89da2596485ecccd29ce47
Jabulani-N <jabu.94@gmail.com>
2024-06-20 11:45:52 -0500

    task 3 created

---
+++ unsupervised_learning/autoencoders/3-variational.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""task 3"""
+
+
+import tensorflow.keras as keras
+
+
+def autoencoder(input_dims, hidden_layers, latent_dims):
+    """placeholder documentation"""
+    pass
