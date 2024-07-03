b454d782bbea0d2e8c43edb55e55b8b765753abb
Jabulani-N <jabu.94@gmail.com>
2024-07-03 12:25:49 -0500

    task 0 removed superfluous dot product calc

--- supervised_learning/RNNs/0-rnn_cell.py
+++ supervised_learning/RNNs/0-rnn_cell.py
@@ -30,10 +30,10 @@ class RNNCell:
     def forward(self, h_prev, x_t):
         """simulates a forward pass"""
         # concatenating these because our cell concatenates inputs and hidden layers
-        catted_input = np.concatenate(x_t, h_prev)
+        catted_input = np.concatenate((x_t, h_prev), axis=1)
         # this will only be a single time step
         # so we'll only use the part protected by the source's loop
-        h_next = np.tanh(np.dot(self.Wh, catted_input) + np.dot(self.bh))
+        h_next = np.tanh(np.dot(self.Wh, catted_input) + self.bh)
         y = np.dot(self.Wy, h_next) + self.by
         return h_next, y
 
