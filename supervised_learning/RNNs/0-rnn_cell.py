e164a7ad939f5e0e4af731c0a88ed690602de7b4
Jabulani-N <jabu.94@gmail.com>
2024-07-03 13:30:18 -0500

    task 0 minor doc update

--- supervised_learning/RNNs/0-rnn_cell.py
+++ supervised_learning/RNNs/0-rnn_cell.py
@@ -28,7 +28,7 @@ class RNNCell:
         self.by = np.zeros((1, o))
 
     def forward(self, h_prev, x_t):
-        """simulates a forward pass"""
+        """simulates a forward propagation pass"""
         # concatenating because our cell concatenates inputs and hidden layers
         # print("shapes:", np.shape(h_prev), np.shape(x_t))
         catted_input = np.concatenate((h_prev, x_t), axis=1)
