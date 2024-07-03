35e54ee3bc1b7a6661a0fa64c3c7f904b121f277
Jabulani-N <jabu.94@gmail.com>
2024-07-03 11:02:24 -0500

    task 0 documentation update

--- supervised_learning/RNNs/0-rnn_cell.py
+++ supervised_learning/RNNs/0-rnn_cell.py
@@ -17,8 +17,10 @@ class RNNCell:
         i, h, o = dimensionality of:
             input, hidden state, output
         """
+        # random NORMAL distribution
         random = np.random.randn
         # weights of the cell
+        # hidden later and input are concatenated here per client request
         self.Wh = random(h + i, h)
         self.Wy = random(h, o)
         # biases of the cell
