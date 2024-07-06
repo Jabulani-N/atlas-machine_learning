task 1 returns correct values, but in wrong shape

supervised_learning/RNNs/1-rnn.py
@@ -12,9 +12,16 @@ def rnn(rnn_cell, X, h_0):
     rnn_cell = already created RNN_Cell class object
         see previous task for attribute explanation
     X = input data
+        shape: (t, m, i)
     h_0 = initial hidden state
+        shape: (m, h)
     """
     H = np.copy(h_0)
-    h_next, output = rnn_cell.forward(h_0, X)
-    H.append(h_next)
-    return H, output
+    h_prev = np.copy(h_0)
+    Y = np.empty(0)
+    for time_step in range(np.shape(X)[0] - 1):
+        h_next, output_next = rnn_cell.forward(h_prev, X[time_step])
+        H = np.append(H, h_next)
+        Y = np.append(Y, output_next)
+        h_prev = h_next
+    return H, np.asarray(Y)