16f88e1b2e72321da1d2d52249cfb747c945429a
Jabulani-N <jabu.94@gmail.com>
2024-07-11 10:09:10 -0500

    task 2 initialized, readme task 2 initialized

---
+++ supervised_learning/RNNs/2-gru_cell.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+This module creates the class RNNCell
+This class illustrates the function of
+a Gated Recurrant  Network
+"""
+
+import numpy as np
+
+
+class GRUCell:
+    """simulates a gated ecurrant network"""
+    pass
