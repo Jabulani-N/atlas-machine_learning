fa8dd77b9ab174b99034a8557552486d6cb2a008
Jabulani-N <jabu.94@gmail.com>
2024-07-15 11:44:34 -0500

    task2 began second required function

--- supervised_learning/RNNs/2-gru_cell.py
+++ supervised_learning/RNNs/2-gru_cell.py
@@ -37,3 +37,9 @@ class GRUCell:
         self.br = np.zeros((1, h))
         self.bh = np.zeros((1, h))
         self.by = np.zeros((1, o))
+
+    def forward(self, h_prev, x_t):
+        """
+        simulates one time step of forward propagation
+        """
+        pass
