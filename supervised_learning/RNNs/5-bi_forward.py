00b54481be17643e2e1b614cbf0daf79ad9039f4
Jabulani-N <jabu.94@gmail.com>
2024-07-16 20:32:08 -0500

    task5 created

---
+++ supervised_learning/RNNs/5-bi_forward.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+This module creates the class BidirectionalCell
+"""
+
+import numpy as np
+
+
+class BidirectionalCell:
+    """simulates a bidirectional cell of an RNN"""
+    def __init__(self, i, h, o):
+        """placeholder doc"""
+        pass
