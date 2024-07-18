e7587012551b0d42e215fd525763398736d87b18
Jabulani-N <jabu.94@gmail.com>
2024-07-17 21:42:37 -0500

    task8 begun

---
+++ supervised_learning/RNNs/8-bi_rnn.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+This module creates the class BidirectionalCell
+this is an update to task 6
+"""
+
+import numpy as np
+
+
+def bi_rnn(bi_cell, X, h_0, h_t):
+    """
+    recieves a cell and states
+    bi_cell = instance of bi-cell class
+    X = given data
+        shape = (t,m,i)
+            t = time steps
+            m = batch size
+            i = dimensionality of input data
+    """
+    pass
