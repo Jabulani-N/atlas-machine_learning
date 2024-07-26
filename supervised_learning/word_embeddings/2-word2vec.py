dcc4d68237cc334d3faaa252638d6f73bcfdfb37
Jabulani-N <jabu.94@gmail.com>
2024-07-25 21:16:17 -0500

    task 2 creates results, but does not match expected test code. testing on checker due to version difference from example code

--- supervised_learning/word_embeddings/2-word2vec.py
+++ supervised_learning/word_embeddings/2-word2vec.py
@@ -4,10 +4,18 @@ This module contains one function
 """
 
 import numpy as np
+from gensim.models import Word2Vec
+from collections.abc import Mapping
 
 
-def word2vec_model(sentences, size=100, min_count=5, window=5, negative=5, cbow=True, iterations=5, seed=0, workers=1):
+def word2vec_model(sentences, size=100, min_count=5,
+                   window=5, negative=5, cbow=True,
+                   iterations=5, seed=0, workers=1):
     """
     looks like it's time to learn about gensim
     """
-    pass
+    model = Word2Vec(sentences=sentences, vector_size=size,
+                     window=window, min_count=min_count,
+                     sg=(not cbow), negative=negative,
+                     epochs=iterations, seed=seed, workers=workers)
+    return model
