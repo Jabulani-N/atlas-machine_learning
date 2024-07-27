4d4c061e030afca2a8bc3ad78d82976ee04104f1
Jabulani-N <jabu.94@gmail.com>
2024-07-27 14:00:48 -0500

    task 3 created

---
+++ supervised_learning/word_embeddings/3-gensim_to_keras.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+word2vec → keras embedding layer
+"""
+
+import numpy as np
+from gensim.models import Word2Vec
+from collections.abc import Mapping
+
+
+def gensim_to_keras(model):
+    """
+    converts a gensim word2vec model
+        to keras embedding layer
+
+    model = trained gensim word2vec model
+    """
+    pass
