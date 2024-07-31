76473b794e9d5dae47be18fbd09d915d7a977797
Jabulani-N <jabu.94@gmail.com>
2024-07-30 21:41:28 -0500

    task 0 passes local test

--- supervised_learning/nlp_metrics/0-uni_bleu.py
+++ supervised_learning/nlp_metrics/0-uni_bleu.py
@@ -14,4 +14,37 @@ def uni_bleu(references, sentence):
 
     sentence = list containing the model proposed sentence
     """
-    pass
+    # prepare list
+    precisions = []
+    for i in range(1, 2):
+        reference_ngrams = collections.Counter()
+        candidate_ngrams = collections.Counter()
+
+        for reference in references:
+            reference_ngrams.update(
+                zip(*[reference[j:] for j in range(i)]))
+
+        candidate_ngrams.update(
+            zip(*[sentence[j:] for j in range(i)]))
+
+        common_ngrams = candidate_ngrams & reference_ngrams
+        precision = sum(common_ngrams.values()) / max(
+            1, sum(candidate_ngrams.values()))
+
+        precisions.append(precision)
+
+    reference_lengths = [len(ref) for ref in references]
+    closest_ref_length = min(
+      reference_lengths,
+      key=lambda x: abs(len(sentence) - x))
+
+    if len(sentence) < closest_ref_length:
+        brevity_penalty = math.exp(1 - closest_ref_length / len(sentence))
+    else:
+        brevity_penalty = 1.0
+
+    bleu_score = brevity_penalty * math.exp(
+        sum(math.log(p) for p in precisions)
+        / len(precisions))
+
+    return bleu_score
