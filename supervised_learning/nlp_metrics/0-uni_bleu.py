bd077725b5bf546a1a9ec7fda4500be768a3dff1
Jabulani-N <jabu.94@gmail.com>
2024-07-30 20:33:34 -0500

    task 0 documented

--- supervised_learning/nlp_metrics/0-uni_bleu.py
+++ supervised_learning/nlp_metrics/0-uni_bleu.py
@@ -1,8 +1,17 @@
 #!/usr/bin/env python3
 
 import numpy as np
+import collections
+import math
 
 
 def uni_bleu(references, sentence):
-    """placeholder doc"""
+    """
+    this is a machine translation metric
+
+    references = list of reference translations
+        each is a list of words in the translation
+
+    sentence = list containing the model proposed sentence
+    """
     pass
