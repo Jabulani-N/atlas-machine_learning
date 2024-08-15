bbed96855a993c2b13317f465266a0fc85c22076
Jabulani-N <jabu.94@gmail.com>
2024-08-15 10:27:51 -0500

    task 4 style, readability improvement

--- supervised_learning/transformer_apps/4-create_masks.py
+++ supervised_learning/transformer_apps/4-create_masks.py
@@ -16,13 +16,13 @@ def pad_mask(inputs, equal_to_me):
     return tf.cast(tf.equal(inputs, equal_to_me), tf.float32)
 
 
-
 def look_mask(kagamine):
     """
     makes a lookahead mask based on len(gth) kagamine
     """
     shape = (kagamine, kagamine)
     mask = 1 - tf.linalg.band_part(tf.ones(shape), -1, 0)
+    return mask
 
 
 def create_masks(inputs, target):
@@ -31,9 +31,11 @@ def create_masks(inputs, target):
     begin by inserting details of what inputs and target are
     """
 
+    _, seq_len_out = tf.shape(target)
     # Create encoder mask
     # uses 0 as padding token
     encoder_mask = pad_mask(inputs, 0)
+    # inputs shape = batch_size, seq_len_in
     # reshape to (batch_size, 1, 1, seq_len_in) as requested
     # this line inserts the "1"s
     encoder_mask = encoder_mask[:, tf.newaxis, tf.newaxis, :]
@@ -43,8 +45,7 @@ def create_masks(inputs, target):
     decoder_mask = pad_mask(target, 0)[:, tf.newaxis, tf.newaxis, :]
 
     # Create the lookahead mask for the decoder
-    seq_len_out = tf.shape(target)[1]
-    lookahead_mask = 1 - tf.linalg.band_part(tf.ones((seq_len_out, seq_len_out)), -1, 0)
+    lookahead_mask = look_mask(seq_len_out)
     # Convert to float for masking  # Shape (seq_len_out, seq_len_out)
     lookahead_mask = tf.cast(lookahead_mask, tf.float32)
 
