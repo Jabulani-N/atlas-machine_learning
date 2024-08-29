cdc637a8fd736d5fd4e4c64f9c88e0a8ea6b6df8
Jabulani-N <jabu.94@gmail.com>
2024-08-29 10:52:21 -0500

    task1 initiatlized

---
+++ reinforcement_learning/q_learning/1-q_init.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+"""returns a gym environment attirbute"""
+
+import gym
+
+
+def q_init(env):
+    """
+    env = FrozenLakeEnv instance
+    Returns:  Q-table as numpy.ndarray of zeros
+    """
+    pass
