a8e13a37a0c63c673d804ec1d7ef4d06a81cfbca
Jabulani-N <jabu.94@gmail.com>
2024-09-02 21:28:16 -0500

    task 2 passes local test

--- reinforcement_learning/q_learning/2-epsilon_greedy.py
+++ reinforcement_learning/q_learning/2-epsilon_greedy.py
@@ -8,7 +8,35 @@ def epsilon_greedy(Q, state, epsilon):
     """
     Q = numpy.ndarray containing q-table
     state = current state
+        int
     epsilon = epsilon used for  calculation
     """
-    # clarity rename
+    # clarity
     qtable = Q
+    _, act_count = np.shape(qtable)
+    state_row = qtable[state]
+
+    greed = np.random.uniform(0.0, 1.0) > epsilon
+    if greed:
+        return greed_is_good(act_count)
+    else:
+        return exploiter_orb(state_row)
+
+
+def greed_is_good(act_count):
+    """
+    recieves a state
+    returns greed route (randomly take a path)
+        aka explore
+    """
+    return np.random.randint(0, act_count)
+
+
+def exploiter_orb(state_row):
+    """
+    recieves a state row
+        of a qtable
+    returns exploit route
+        = index of highest known reward
+    """
+    return np.argmax(state_row)
