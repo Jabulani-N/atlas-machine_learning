b00b9fe7f1ca0c2b484b8cbd9e25bcd2738ca110
Jabulani-N <jabu.94@gmail.com>
2024-09-09 18:36:09 -0500

    task 0 creates results, but they are not as expected, and break expected range of 1. maximum. likely a 'forgot to average the sum' issue somewhere

--- reinforcement_learning/temporal_difference/0-monte_carlo.py
+++ reinforcement_learning/temporal_difference/0-monte_carlo.py
@@ -1,11 +1,54 @@
 #!/usr/bin/env python3
 """performs the Monte Carlo algorithm"""
 
-import None
+import numpy as np
 
 
 def monte_carlo(env, V, policy,
                 episodes=5000, max_steps=100,
                 alpha=0.1, gamma=0.99):
-    """placeholder doc"""
-    pass
+    """
+    env = environment instance
+        tested with frozenlake
+    V = Value Estimate
+        np.ndarray
+            shape (s,)
+    policy = policy function
+        function
+            recieves state
+                returns next action from state
+    episodes = number of episodes to train over
+        int
+    max_steps = steps per episode
+    alpha = learning rate
+    gamma = discount rate
+
+    Returns:
+        V = value estimate (upated)
+    """
+    num_states = env.observation_space.n
+    # capitalized to make distinct from later variables
+    returns = {State: [] for State in range(num_states)}
+
+    for episode in range(episodes):
+        state = env.reset()
+        episode_data = []
+        # Generate an episode
+        for step in range(max_steps):
+            action = policy(state)
+            next_state, reward, terminal, _ = env.step(action)
+            episode_data.append((state, reward))
+            state = next_state
+            if terminal:
+                break
+
+        # Calculate return,  update value table estimates
+        G = 0
+        for state, reward in reversed(episode_data):
+            G = gamma * G + reward
+
+            returns[state].append(G)
+            # V[state] += alpha * (G - V[state])
+            V[state] += alpha * np.mean(returns[state])
+
+    return V
--- reinforcement_learning/temporal_difference/README.md
+++ reinforcement_learning/temporal_difference/README.md
@@ -10,3 +10,15 @@
 ](https://medium.com/@violante.andre/simple-reinforcement-learning-temporal-difference-learning-e883ea0d65b0) (medium member only)
 
 [On-Policy TD control methods](https://paperswithcode.com/methods/category/on-policy-td-control)
+
+### [Monte Carlo Policy evaluation](https://www.geeksforgeeks.org/monte-carlo-policy-evaluation/)
+
+Evaluation Paent formula
+
+* ![evaluation parent formula](https://quicklatex.com/cache3/30/ql_456afe0ff1ee40f5503c31f1dfb79030_l3.svg)
+  * `V` is "value" of state `s` under policy `π`.
+  * `G` = average return of that state `s` using that policy.
+  * `N(s)` = number of times state `s` is visited during episode.
+  * return is `V(s)` value of that state `s`
+
+That is the general logic we'll be following to create the monte carlo policy evaluation.
