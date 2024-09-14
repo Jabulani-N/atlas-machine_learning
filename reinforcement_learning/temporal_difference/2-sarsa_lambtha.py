189ba7b60a1554c74a445539ee4eda54af5a8210
Jabulani-N <jabu.94@gmail.com>
2024-09-14 15:24:51 -0500

    doc updated

--- reinforcement_learning/temporal_difference/2-sarsa_lambtha.py
+++ reinforcement_learning/temporal_difference/2-sarsa_lambtha.py
@@ -9,5 +9,21 @@ def sarsa_lambtha(env, Q, lambtha, episodes=5000, max_steps=100,
                   min_epsilon=0.1, epsilon_decay=0.05):
     """
     env = openai environment instance
+    Q = qtable
+        np.ndarray
+            shape = (s,a)
+                s = number of states
+                a = number of actions
+    lambtha = eligibility trace factor
+    episodes = num episodes for training
+    max_steps = steps per episode
+    alpha = learning rate
+    gamma = discount rate
+    epsilon = initial threshold for epsilon greedy
+        float
+    epsilon_decay = how much to decrease epsilon between episodes
+    min_epsilon = epsilon must go below this value
+
+    return Q (updated)
     """
     pass
