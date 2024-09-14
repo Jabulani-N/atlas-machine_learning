69f90b465568cfebebe82071f7bedb0f193f66ca
Jabulani-N <jabu.94@gmail.com>
2024-09-14 14:53:11 -0500

    minor doc update

--- reinforcement_learning/temporal_difference/2-sarsa_lambtha.py
+++ reinforcement_learning/temporal_difference/2-sarsa_lambtha.py
@@ -7,5 +7,7 @@ import numpy as np
 def sarsa_lambtha(env, Q, lambtha, episodes=5000, max_steps=100,
                   alpha=0.1, gamma=0.99, epsilon=1,
                   min_epsilon=0.1, epsilon_decay=0.05):
-    """placeholder documentation"""
+    """
+    env = openai environment instance
+    """
     pass
