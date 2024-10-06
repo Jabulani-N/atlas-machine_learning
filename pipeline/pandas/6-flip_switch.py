b7f74eee026fd71d609e529776914fea3449d34b
Jabulani-N <jabu.94@gmail.com>
2024-10-06 17:48:40 -0500

    task 6 passes local test

--- pipeline/pandas/6-flip_switch.py
+++ pipeline/pandas/6-flip_switch.py
@@ -2,8 +2,9 @@
 """
 pandas dataframe manipulation
 
-transposes rows and columns
-sorts resultant dataframe
+sorts dataframe
+transposes dataframe
+    rotates so columns become rows & vice-versa
 """
 
 import pandas as pd
@@ -11,6 +12,6 @@ from_file = __import__('2-from_file').from_file
 
 df = from_file('coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv', ',')
 
-df =  # YOUR CODE HERE
+df = df.sort_values(by="Timestamp", ascending=False).transpose()
 
 print(df.tail(8))
