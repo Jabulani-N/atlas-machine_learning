e44c6529de1f08172d4be90e6d86feabeccdf489
Jabulani-N <jabu.94@gmail.com>
2024-10-15 19:12:31 -0500

    task 1 now creates sentient species list

--- pipeline/apis/1-sentience.py
+++ pipeline/apis/1-sentience.py
@@ -14,12 +14,31 @@ def sentientPlanets():
     differs from task 0 as the conditoins have conditions
         essentially, we'll have nest
 
-    list all planets
     list all sapient species
     list all planets that have somehting from sapient species list
         for specie in planet's specieses
             if in sapient species list
                 say "sapient planet" and break
                     break because don't care about remaining entries on planet
+
+    ideally, we'd limit api call count
+        but first make it work at all
     """
-    pass
+    sent_spec_list = []
+    planet_list = []
+
+    # list of all sw species
+    url = 'https://swapi-api.hbtn.io/api/species/'
+    relevant_names = []
+    # get sentient species
+    while url:
+        response = requests.get(url)
+        # data actually has the usable data dict
+        data = response.json()
+        # swapi has results under 'results' key
+        for entry in data["results"]:
+            if entry['designation'] == "sentient":
+                sent_spec_list.append(entry["name"])
+        url = data['next']
+
+    return relevant_names
