8a015249db318db145d5fd0ba868c0944fa74c65
Jabulani-N <jabu.94@gmail.com>
2024-10-23 19:16:38 -0500

    task 8 shows show id alongside genre id

---
+++ pipeline/databases/8-genre_id_by_show.sql
@@ -0,0 +1,5 @@
+-- for each row in tv_show_genres,
+--  get title  of corresponding id from tv_shows
+SELECT tv_shows.id AS show_id, tv_show_genres.genre_id AS genre_id
+FROM tv_shows
+INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.genre_id;
