f439b9ab681c69a449a7404685cdff5a2bc921d0
Jabulani-N <jabu.94@gmail.com>
2024-10-23 19:22:48 -0500

    task 8 now orders by title and genre id, now passes local test

--- pipeline/databases/8-genre_id_by_show.sql
+++ pipeline/databases/8-genre_id_by_show.sql
@@ -2,4 +2,5 @@
 --  get title  of corresponding id from tv_shows
 SELECT tv_shows.title, tv_show_genres.genre_id AS genre_id
 FROM tv_shows
-INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.genre_id;
+INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
