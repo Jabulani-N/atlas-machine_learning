6e2e7670859c0d65776d34473702574a5bb3ee29
Jabulani-N <jabu.94@gmail.com>
2024-10-23 22:47:51 -0500

    task 10 in order, corrected column 1 name to genre

--- pipeline/databases/10-count_shows_by_genre.sql
+++ pipeline/databases/10-count_shows_by_genre.sql
@@ -1,7 +1,7 @@
 -- counts how many shows are in each genre
-SELECT tv_genres.name, COUNT(tv_show_genres.show_id) AS number_of_shows
+SELECT tv_genres.name as genre, COUNT(tv_show_genres.show_id) AS number_of_shows
 FROM tv_genres
 LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 -- WHERE tv_show_genres.genre_id IS NULL
-GROUP BY tv_genres.name;
-ORDER BY tv_genres.name;
+GROUP BY tv_genres.name
+ORDER BY number_of_shows desc;
