b12257e69a6a3b408e910cec275abab05086c95c
Jabulani-N <jabu.94@gmail.com>
2024-10-24 23:11:33 -0500

    task 18 logic is present, but syntax is not

---
+++ pipeline/databases/18-valid_email.sql
@@ -0,0 +1,10 @@
+-- if email is updated AND CHANGED, resets valid_email
+CREATE TRIGGER reset_valid_email
+BEFORE UPDATE ON users
+FOR EACH ROW
+BEGIN
+    IF NEW.email <> OLD.email THEN
+        SET NEW.valid_email = 0  -- the reset
+    END IF;
+END
+;
