424caf2b4bf38b0995ddcbaf187a71a6ca467de5
Jabulani-N <jabu.94@gmail.com>
2024-10-25 02:05:39 -0500

    task 30 created

---
+++ pipeline/databases/30-all.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+lists all document in provided mongo collection
+"""
+
+import pymongo
+
+
+def list_all(mongo_collection):
+    """
+    mongo_collection = mongo collection
+
+    lists all documents
+    """
+    listy = []
+    for document in mongo_collection:
+        list.append(document)
+    return listy
