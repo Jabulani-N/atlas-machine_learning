8ce58227f5becf1798f51a2aa5f21fc930ea7867
Jabulani-N <jabu.94@gmail.com>
2024-10-24 23:17:03 -0500

    task 18 syntax fixed, using begin block and delimiter, passes local test

--- pipeline/databases/18-valid_email.sql
+++ pipeline/databases/18-valid_email.sql
@@ -5,7 +5,7 @@ BEFORE UPDATE ON users
 FOR EACH ROW
 BEGIN
     IF NEW.email <> OLD.email THEN
-        SET NEW.valid_email = 0  -- the reset
+        SET NEW.valid_email = 0;  -- the reset
     END IF;
 END
 ;
