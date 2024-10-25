9e6d4fe077abaeb375961729b589713023878136
Jabulani-N <jabu.94@gmail.com>
2024-10-24 22:16:16 -0500

    task 17 removed begin,end, now passes local test

--- pipeline/databases/17-store.sql
+++ pipeline/databases/17-store.sql
@@ -2,8 +2,8 @@
 CREATE TRIGGER decrease_item_quantity
 AFTER INSERT ON orders
 FOR EACH ROW
-BEGIN
+
     UPDATE items
     SET quantity = quantity - NEW.number
-    WHERE items.name = NEW.item_name;
-END;
+    WHERE items.name = NEW.item_name
+;
